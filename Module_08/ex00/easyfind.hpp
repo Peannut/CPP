22f49bbd32c34023e375daef10ec64c849a6f6d4
Peannut <zakariaspam0@gmail.com>
2022-12-23 22:22:11 +0100

    tal ghda o nkml

---
+++ Module_08/ex00/easyfind.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zoukaddo <zoukaddo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/23 22:14:56 by zoukaddo          #+#    #+#             */
+/*   Updated: 2022/12/23 22:19:14 by zoukaddo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _EASYFIND_HPP_
+# define  _EASYFIND_HPP_
+
+template <class T>
+int& easyfind(T& container, int to_find)
+{
+	
+}
+#endif 
